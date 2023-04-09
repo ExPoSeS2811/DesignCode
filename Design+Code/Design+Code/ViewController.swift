032b519498ce21e231d50266f9427654579b7515
Gleb Rasskazov <glebunium146@gmail.com>
2023-04-09 14:05:17 +0300

    Added design interface Added card view Added setting navigation + tab bar

---
+++ Design+Code/Design+Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Design+Code
+//
+//  Created by MacBook Pro 13 2019 on 4/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
