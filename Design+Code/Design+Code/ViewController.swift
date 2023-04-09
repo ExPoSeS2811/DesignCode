97e3625b64e0c4baf2efde3898c0009290b392f2
Gleb Rasskazov <glebunium146@gmail.com>
2023-04-09 15:34:05 +0300

    Finished design card view Added shadow Added corner radius Added helpers

--- Design+Code/Design+Code/ViewController.swift
+++ Design+Code/Design+Code/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var cardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cardView.layer.cornerRadius = 30
+        cardView.layer.cornerCurve = .continuous
+        blurView.layer.cornerRadius = 30
+        blurView.layer.cornerCurve = .continuous
+
+        blurView.layer.masksToBounds = true
+        
+        cardView.layer.shadowColor = UIColor(named: "Shadow")!.cgColor
+        cardView.layer.shadowOpacity = 0.5
+        cardView.layer.shadowRadius = 20
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 10)
+
     }
 
 
